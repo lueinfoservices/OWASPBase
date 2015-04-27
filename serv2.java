3f5b507bea668a92e235daed5bc1cdf8d7ca55fd
Mamnoon Hadi <mamnoonhadi@Lenovo-P>
2015-04-27 17:46:25 +0100

    servlets

---
+++ serv2.java
@@ -0,0 +1,46 @@
+
+
+public class OWASP1 extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+            if (OWASP.equalsIgnoreCase("Injection Flaws")) {
+                RequestDispatcher rd = request.getRequestDispatcher("injection.jsp");
+                rd.forward(request, response);
+
+            } else if (OWASP.equalsIgnoreCase("Broken Authentication and Session Management")) {
+                RequestDispatcher rd = request.getRequestDispatcher("brokenauthentication.jsp");
+                rd.forward(request, response);
+            } else if (OWASP.equalsIgnoreCase("Cross-Site Scripting")) {
+                RequestDispatcher rd = request.getRequestDispatcher("crosssite.jsp");
+                rd.forward(request, response);
+            } else if (OWASP.equalsIgnoreCase("Insecure Direct Object References")) {
+                RequestDispatcher rd = request.getRequestDispatcher("insecuredirect.jsp");
+                rd.forward(request, response);
+            } else if (OWASP.equalsIgnoreCase("Security Misconfiguration")) {
+                RequestDispatcher rd = request.getRequestDispatcher("securitymisconfiguration.jsp");
+                rd.forward(request, response);
+            } else if (OWASP.equalsIgnoreCase("Sensitive Data Exposure")) {
+                RequestDispatcher rd = request.getRequestDispatcher("sensitivedataexposure.jsp");
+                rd.forward(request, response);
+            } else if (OWASP.equalsIgnoreCase("Missing Function Level Access Control")) {
+                RequestDispatcher rd = request.getRequestDispatcher("missingfunctionlevel.jsp");
+                rd.forward(request, response);
+            } else if (OWASP.equalsIgnoreCase("Cross-Site Request Forgery (CSRF)")) {
+                RequestDispatcher rd = request.getRequestDispatcher("csrf.jsp");
+                rd.forward(request, response);
+            } else if (OWASP.equalsIgnoreCase("Using Known Vulnerable Components")) {
+                RequestDispatcher rd = request.getRequestDispatcher("usingknown.jsp");
+                rd.forward(request, response);
+            } else if (OWASP.equalsIgnoreCase("Unvalidated Redirects and Forwards")) {
+                RequestDispatcher rd = request.getRequestDispatcher("unvalidatedredirects.jsp");
+                rd.forward(request, response);
+            } else {
+                RequestDispatcher rd = request.getRequestDispatcher("error.jsp");
+                rd.forward(request, response);
+            }
+        }
+    }
+}
